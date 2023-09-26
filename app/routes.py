c366edc5be2423ef951eb450cb6da31791d502dc
Mrumbutsa-Marion <marion.mwanje@student.moringaschool.com>
2023-09-26 19:34:32 +0300

    Creating the second Get request

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app, db
 from flask import request, jsonify, make_response
-from app.models import Restaurant
+from app.models import Restaurant, Pizza
 
 @app.route('/restaurants', methods=['GET'])
 def get_restaurants():
@@ -20,5 +20,49 @@ def get_restaurants():
     return jsonify(restaurant_list)
 
 
-# Seed the database
 
+@app.route('/restaurants/<int:id>')
+def get_restaurants_by_id(id):
+    restaurants = Restaurant.query.get(id)
+    if not restaurants:
+        return jsonify({"error": "Restaurant not found"}), 404
+    
+    # Create a list that will store the pizza data inside the restaurants.pizzas
+    pizzas = []
+    for pizza in restaurants.pizzas:
+        pizza_data = {
+            'id': pizza.id,
+            'name': pizza.name,
+            'ingredients': pizza.ingredients
+        }
+        pizzas.append(pizza_data)
+
+    restaurant_data = {
+        'id': restaurant.id,
+        'name': restaurant.name,
+        'address': restaurant.address,
+        'pizzas': pizzas
+    }
+
+    return jsonify(restaurant_data)
+# Set up the application context
+with app.app_context():
+    restaurants = [
+        Restaurant(name='Dominion Pizza', address='Good Italian, Ngong Road, 5th Avenue'),
+        Restaurant(name='Pizza Hut', address='Westgate Mall, Mwanzi Road, Nrb 100'),
+        Restaurant(name='Mcdonald Pizaa', address='Imaradaima Mall, Mombasa rd')
+    ]
+
+    # Check if the third restaurant already exists in the database
+    restaurant = Restaurant.query.filter_by(name=restaurants[2].name).first()
+
+    # If the third restaurant does not exist in the database, add it
+    if restaurant is None:
+        db.session.add(restaurants[2])
+    else:
+        # If the third restaurant already exists in the database, do nothing
+        pass
+
+    db.session.commit()
+
+print('Database seeded successfully.')
